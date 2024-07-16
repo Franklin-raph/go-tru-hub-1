71024d9c17be8e822d0050eccdac9a28d4470887
franklin-raph <igboekwulusifranklin@gmail.com>
2024-07-16 16:07:41 +0100

    update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+
+  const navigate = useNavigate()
+  
   return (
     <>
       <Navbar />
